7e3bd19018dfe9546281dac6945d34260fc1b60a
cshruti87 <cshruti87@yahoo.com>
2023-03-30 09:47:08 -0400

    delimiter

--- ssg/content.py
+++ ssg/content.py
@@ -3,8 +3,8 @@ from yaml import load, FullLoader
 from collections.abc import Mapping
 
 class Content(Mapping):
-    __delimeter = "^(?:-|\+){3}\s*$"
-    __regex = re.compile(__delimeter, re.MULTILINE)
+    __delimiter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
 
     @classmethod
     def load(cls, string):
